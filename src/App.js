1cfd8cab338a3ad66563797fbd60f54e3130a5c7
phershbe <phershbe@gmail.com>
2021-08-27 18:25:22 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [data, setData] = useState('Data goes here');
+  const [API, setAPI] = useState('Data from API goes here');
+
+  const readUsername = (e) => {
+    setUsername(e.target.value);
+  };
+
+  const readPassword = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const inputData = {
+      username: username,
+      password: password,
+    };
+    const requestCall = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(inputData),
+    };
+    await fetch('http://localhost:8080/register', requestCall);
+    alert('Thank you for your submission!');
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello World!</h1>
+      <form onSubmit={handleSubmit} method="POST">
+        <label htmlFor="username" name="username" >Username: </label>
+        <input htmlFor="username" name="username" onChange={readUsername} />
+        <br/>
+        <label htmlFor="password" name="password" >Password: </label>
+        <input htmlFor="password" name="password" type="password" onChange={readPassword} />
+        <br/>
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 }
